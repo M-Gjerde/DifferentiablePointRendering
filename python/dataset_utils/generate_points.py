37aa27bcd225e018641496b7de8bb4e0f49f8f70
magnus <gjerde.magnus@gmail.com>
2026-04-24 16:38:40 +0200

    small adjust

--- python/dataset_utils/generate_points.py
+++ python/dataset_utils/generate_points.py
@@ -434,7 +434,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--max-z", type=float)
 
     parser.add_argument("--scale", type=float)
-    parser.add_argument("--opacity", type=float, default=0.5)
+    parser.add_argument("--opacity", type=float, default=0.0)
     parser.add_argument("--position-noise-std", type=float)
     parser.add_argument("--tangent-noise-std", type=float)
     parser.add_argument("--seed", type=int, default=None)
